7b409d6eea3ada050f51221875cab125c6140f4a
jade-nl <jade_nl@ziggo.nl>
2019-12-12 13:34:38 +0100

    Show data

--- bin/dt.cfg.sh
+++ bin/dt.cfg.sh
@@ -13,7 +13,9 @@
 # -------------------------------------------------------------------------- #
 # Dependencies : default config file : /opt/dt.bldr/cfg/dt.bldr.cfg
 # -------------------------------------------------------------------------- #
-# Changes      : oct 30 2019 - First build / outline           1.0.0-alpha.1
+# Changes      : oct 28 2019 - First build                       1.0.0-alpha
+#              : oct 31 2019 - Import data                     1.0.0-alpha.1
+#              : nov 02 2019 - Show data                       1.0.0-alpha.2
 # -------------------------------------------------------------------------- #
 #set -xv
 set -u
@@ -22,7 +24,7 @@ umask 026
 # --- Variables ---
 # ------------------------------------------------------------------ #
 # script core
-scriptVersion="1.0.0-alpha.1"
+scriptVersion="1.0.0-alpha.2"
 scriptName="$(basename ${0})"
 # script directories
 scriptDir="/opt/dt.bldr"
@@ -31,10 +33,16 @@ cfgDir="${scriptDir}/cfg"
 # script files
 defCfgFile="${cfgDir}/dt.bldr.cfg"
 usrCfgFile="$HOME/.config/dt.bldr/dt.bldr.cfg"
+# arrays
+declare -A optsDIRS
+declare -A optsCMAKE
+declare -A optsUSE
+declare -A optsBUILD
+declare -A optsDFLT
+declare -A optsCRS
 # general
 doChkFile="0"
 doShwFile="0"
-
 
 # -------------------------------------------------------------------------- #
 # --- Functions ---
@@ -53,73 +61,94 @@ function _doCheck ()
 # ------------------------------------------------------------------ #
 function _doShow ()
 {
-  awk -v defcfgfile="${defCfgFile}" '
-  BEGIN { 
-    # set field separator
-    FS = "[ =]"
-    # read default configuration file and store relevant parts
-    while ( ( getline < defcfgfile ) > 0 )
-      { gsub("\"","") 
-        if ( $1 ~ /Dir/ )     optsDIRS[$1]  = " D "$2 
-        if ( $1 ~ /^CMAKE_/ ) optsCMAKE[$1] = " D "$2  
-        if ( $1 ~ /USE_/ )    optsUSE[$1]   = " D "$2  
-        if ( $1 ~ /BUILD_/ )  optsBUILD[$1] = " D "$2  
-        if ( $1 ~ /_BUILD/ )  optsBUILD[$1] = " D "$2  
-        if ( $1 ~ /CUSTOM_/ ) optsBUILD[$1] = " D "$2  
-        if ( $1 ~ /^dflt/ )   optsDFLT[$1]  = " D "$2  
-        if ( $1 ~ /^crs/ )    optsCRS[$1]   = " D "$2 }
-  }
-  {
-    # replace default with available user entries
-      { gsub("\"","") }
-      { if ( $1 ~ /Dir/ )     optsDIRS[$1]  = " U "$2 }
-      { if ( $1 ~ /^CMAKE_/ ) optsCMAKE[$1] = " U "$2 }
-      { if ( $1 ~ /USE_/ )    optsUSE[$1]   = " U "$2 }
-      { if ( $1 ~ /BUILD_/ )  optsBUILD[$1] = " U "$2 }
-      { if ( $1 ~ /_BUILD/ )  optsBUILD[$1] = " U "$2 }
-      { if ( $1 ~ /CUSTOM_/ ) optsBUILD[$1] = " U "$2 }
-      { if ( $1 ~ /^dflt/ )   optsDFLT[$1]  = " U "$2 }
-      { if ( $1 ~ /^crs/ )    optsCRS[$1]   = " U "$2 }
-  }
-  END {
-      # show what has been found
-   { print "# ---------------------------------------------------------------------------- #" }
-   {  printf "%-25s %5s %s\n", "#  Option", "D/U", "Value" }
-   { print "# ---------------------------------------------------------------------------- #" }
-   { print "# - directories -" }
-   { print "# ----------------------------------------------------------------------- #" }
-   for (key in optsDIRS)  { printf "# %-25s %s\n", key, optsDIRS[key] }
-   { print "# ----------------------------------------------------------------------- #" }
-   { print "# - cmake options -" }
-   { print "# ----------------------------------------------------------------------- #" }
-   for (key in optsCMAKE) { printf "# %-25s %s\n", key, optsCMAKE[key] }
-   { print "# ----------------------------------------------------------------------- #" }
-   { print "# - use options -" }
-   { print "# ----------------------------------------------------------------------- #" }
-   for (key in optsUSE)   { printf "# %-25s %s\n", key, optsUSE[key] }
-   { print "# ----------------------------------------------------------------------- #" }
-   { print "# - build options -" }
-   { print "# ----------------------------------------------------------------------- #" }
-   for (key in optsBUILD) { printf "# %-25s %s\n", key, optsBUILD[key] }
-   { print "# ----------------------------------------------------------------------- #" }
-   { print "# - run options -" }
-   { print "# ----------------------------------------------------------------------- #" }
-   for (key in optsDFLT)  { printf "# %-25s %s\n", key, optsDFLT[key] }
-   { print "# ----------------------------------------------------------------------- #" }
-   { print "# - cpu cores -" }
-   { print "# ---------------------------------------------------------------------------- #" }
-   for (key in optsCRS)   { printf "# %-25s %s\n", key, optsCRS[key] }
-   { print "# ---------------------------------------------------------------------------- #" }
+  
+  echo "# -------------------------------------------------- directories --- #"
+  for key in "${!optsDIRS[@]}"; do
+      printf "#  %-28s%-5s%s\n" ${key} ${optsDIRS[$key]}
+  done | sort
+  
+  echo "# ------------------------------------------------ cmake options --- #"
+  for key in "${!optsCMAKE[@]}"; do
+      printf "#  %-28s%-5s%s\n" ${key} ${optsCMAKE[$key]}
+  done | sort
+  
+  echo "# -------------------------------------------------- use options --- #"
+  for key in "${!optsUSE[@]}"; do
+      printf "#  %-28s%-5s%s\n" ${key} ${optsUSE[$key]}
+  done | sort
+  
+  echo "# ------------------------------------------------ build options --- #"
+  for key in "${!optsBUILD[@]}"; do
+      printf "#  %-28s%-5s%s\n" ${key} ${optsBUILD[$key]}
+  done | sort
+  
+  echo "# ------------------------------------------------- run defaults --- #"
+  for key in "${!optsDFLT[@]}"; do
+      printf "#  %-28s%-5s%s\n" ${key} ${optsDFLT[$key]}
+  done | sort
+  
+  echo "# -------------------------------------------------- cpu options --- #"
+  for key in "${!optsCRS[@]}"; do
+      printf "#  %-28s%-5s%s\n" ${key} ${optsCRS[$key]}
+  done | sort
+  
+  echo "# ------------------------------------------------------------------ #"
+}
 
-  }' "${usrCfgFile}"
+# -------------------------------------------------------------------------- #
+# --- Functions ---
+# ------------------------------------------------------------------ #
+# Function : Show error and exit
+# Syntax   : _errHndlr
+# ------------------------------------------------------------------ #
+function _errHndlr ()
+{
+  # legible
+  errorLocation="$1"
+  errorMessage="$2"
+  # show error
+  echo "
+  A fatal error occured.
+
+    Script   : ${scriptName} (${scriptVersion})
+    Function : ${errorLocation}
+    Error    : ${errorMessage}
+
+    For possible details :  ${cmkBldLog}
+
+  Exiting now.
+"
+  exit 128
 }
 
 # -------------------------------------------------------------------------- #
 # --- Main ---
 # ------------------------------------------------------------------ #
 clear
-# do not run script as root
-#[ "$EUID" -eq 0 ] && echo "Do not run as root user!" && exit 255
+# some basic checks
+[ "$EUID" -eq 0 ]      && _errHndlr "Basic checks" "Do not run as root user."
+[ ! -f ${defCfgFile} ] && _errHndlr "Basic checks" "${defCfgFile} does not exist."
+# -------------------------------------------------------- #
+# parse both cfg files
+while IFS=$'\n' read -r OPTVAL
+do
+  # process input line
+  OPTVAL=${OPTVAL//\"/} # strip double quotes
+  cfgOPT=${OPTVAL%=*}   # all before =
+  cfgVal=${OPTVAL#*=}   # all after =
+  # store opt + val pair in appropriate array
+  [[ ${cfgOPT} =~ Dir ]]         && optsDIRS[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^CMAKE_ ]]     && optsCMAKE[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^USE_ ]]       && optsUSE[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^DONT_ ]]      && optsUSE[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^BUILD_ ]]     && optsBUILD[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^CUSTOM ]]     && optsBUILD[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^BINARY ]]     && optsBUILD[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^TESTBUILD_ ]] && optsBUILD[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^dflt ]]       && optsDFLT[${cfgOPT}]=${cfgVal}
+  [[ ${cfgOPT} =~ ^crs ]]        && optsCRS[${cfgOPT}]=${cfgVal}
+done < <(cat ${defCfgFile} ${usrCfgFile} 2>/dev/null)
+
 # handle options given
 while getopts "cs" OPTION
 do
